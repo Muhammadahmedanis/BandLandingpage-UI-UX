28df23b142764e5f8d1948fbc2d8d3ffdeea921b
ahmedanis45 <ahmedanis4546@gmail.com>
2024-04-28 22:19:00 +0500

    update functioning

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 function cli() {
     var a = document.querySelector(".show");
     var btn = document.querySelector(".clickbtn");
-    if(btn.innerHTML === "See more"){
+    if(btn.innerText === "See more"){
         a.style.display = "block";
         a.style.display  ="flex";
         btn.innerHTML = "See less"
-    }else if (btn.innerHTML == "See less") {
+    }else if (btn.innerText == "See less") {
         a.style.display = "none";
-        btn.innerHTML = "See More";
+        btn.innerHTML = "See more";
     }
 }
 
+var bg = document.querySelector("#main");
 
 var lName = document.querySelector("#lName");
 var email = document.querySelector("#email");
@@ -18,20 +19,24 @@ var passward = document.querySelector("#passward");
 var conPassward = document.querySelector("#conPassward");
 var fName = document.querySelector("#fName");
 function sub(){
-    if(passward.length != 8) {
-        passward.value  = "Ypour"
-    }
+    // if(passward.length != 8) {
+    //     passward.value  = "Ypour"
+    // }
     console.log(fName.value, lName.value, email.value, passward.value, conPassward.value);
     fName.value = "";
     lName.value = "";
     email.value = "";
     passward.value = "";
     conPassward.value = "";
+    var formHide = document.querySelector(".sign");
+    formHide.style.display = "none"; 
+    bg.style.backgroundColor = "#F0F0F0";
 }
 
 function signUp() {
     // console.log("a");
     var a = document.querySelector(".sign")
     a.style.display = "block";
+    bg.style.backgroundColor = "darkslategray";
 }
 
