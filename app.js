697788b4766c485ed113676437adbec6bbbfac04
ahmedanis45 <ahmedanis4546@gmail.com>
2024-04-30 21:23:36 +0500

    update JS

--- app.js
+++ app.js
@@ -18,16 +18,22 @@ var email = document.querySelector("#email");
 var passward = document.querySelector("#passward");
 var conPassward = document.querySelector("#conPassward");
 var fName = document.querySelector("#fName");
+
+var c = document.querySelector(".check");
+
 function sub(){
-    // if(passward.length != 8) {
-    //     passward.value  = "Ypour"
-    // }
+    if(passward.value.length > 8) {
+        c.disabled = true;
+    }else {
+        c.disabled = false;
+    }
     console.log(fName.value, lName.value, email.value, passward.value, conPassward.value);
     fName.value = "";
     lName.value = "";
     email.value = "";
     passward.value = "";
     conPassward.value = "";
+
     var formHide = document.querySelector(".sign");
     formHide.style.display = "none"; 
     bg.style.backgroundColor = "#F0F0F0";
@@ -35,6 +41,7 @@ function sub(){
 
 function signUp() {
     // console.log("a");
+    // c.disabled = true;
     var a = document.querySelector(".sign")
     a.style.display = "block";
     bg.style.backgroundColor = "darkslategray";
